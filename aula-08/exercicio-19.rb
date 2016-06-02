f640a3977bea311836b14d564828ca1943c574e2
Felipe Pontes <felipemfpontes@gmail.com>
2016-06-02 14:29:04 -0300

    Update exercicio-19.rb

--- aula-08/exercicio-19.rb
+++ aula-08/exercicio-19.rb
@@ -7,21 +7,19 @@ distancia = gets.to_i # km
 # S = S0 + t * v
 
 if c2_velocidade > c1_velocidade
-    puts '--:--:--'
-    exit
+ 	r = '--:--:--'
+else
+	tempo = (distancia * 1.0) / (c1_velocidade - c2_velocidade)
+	if tempo * c1_consumo > c1_tanque
+	    r = '--:--:--'
+	else
+		s = tempo * (60.0 * 60.0)
+		m = s.to_i / 60
+		s -= m * 60
+		h = m / 60
+		m -= h * 60
+		r = '%02.0f:%02.0f:%02.0f' % [h, m, s]
+	end
 end
 
-tempo = (distancia * 1.0) / (c1_velocidade - c2_velocidade)
-
-if tempo * c1_consumo > c1_tanque
-    puts '--:--:--'
-    exit
-end
-
-s = tempo * (60.0 * 60.0)
-m = s.to_i / 60
-s -= m * 60
-h = m / 60
-m -= h * 60
-
-puts '%02.0f:%02.0f:%02.0f' % [h, m, s]
+puts r
