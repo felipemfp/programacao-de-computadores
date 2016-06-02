a3e3e218f790fac197b4bcc12908ccf8a2d5c439
Felipe Pontes <felipemfpontes@gmail.com>
2016-06-02 17:43:51 -0300

    Update exercicio-18.rb

--- aula-08/exercicio-18.rb
+++ aula-08/exercicio-18.rb
@@ -13,9 +13,10 @@ final_grade = partial_grade
 
 if partial_grade >= 2 and partial_grade < 6
 	final_test = gets.to_f
-	final_grade = (partial_grade + final_test) / 2
-	final_grade = (final_grade < 6) ? ((2*grade1 + 3*final_test) / 5) : final_grade
-	final_grade = (final_grade < 6) ? ((3*grade2 + 2*final_test) / 5) : final_grade
+	final_grade1 = ((partial_grade + final_test) / 2)
+	final_grade2 = ((2*grade1 + 3*final_test) / 5)
+	final_grade3 = ((3*grade2 + 2*final_test) / 5) 
+	final_grade = (final_grade1 > final_grade2 and final_grade2 > final_grade3) ? final_grade1 : (final_grade2 > final_grade3 ? final_grade2 : final_grade3)
 end
 
 final_test_report = final_test ? '%.1f' % final_test : '-'
